79059adc4f224c506210612f3499712bca52d24e
k <k@ka.(none)>
2013-10-08 11:47:49 +0200

    take into account the case that the first sentence is shorter than the second one

--- get_difference.rb
+++ get_difference.rb
@@ -1,8 +1,16 @@
 module GetDifference
 	def self.get_difference a, b
 	  difference = 0
-	  a.split("").each_with_index do |c, i|
-	    if c != b[i]
+	  
+	  if a.length > b.length
+	  	longer_sentence = a
+	  	shorter_sentence = b
+	  else 
+	  	longer_sentence = b
+	  	shorter_sentence = a 
+	  end
+	  longer_sentence.split("").each_with_index do |c, i|
+	    if c != shorter_sentence[i]
 	    	difference += 1
 	    end
 	  end
